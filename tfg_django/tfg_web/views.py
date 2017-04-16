8e88b4cc7d3ce47fba8451ca1dbc8484da6e2450
muscraziest <ltiradolopz@gmail.com>
2017-04-17 00:35:41 +0200

    Add files via upload

--- tfg_django/tfg_web/views.py
+++ tfg_django/tfg_web/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponse, render_to_response
 from . forms import Formulario
 from . lanzarModulos import modulos
 from django.conf import settings
+from django.contrib import messages
 
 #Funcion para procesar las opciones elegidas por el usuario y lanzar el sistema experto
 def enviarDatos(request):
@@ -17,13 +18,16 @@ def enviarDatos(request):
 
 			#Comprobamos que la extension es correcta
 			if not partitura.name.endswith('.xml'):
-				return render(request,'error.html')
+				messages.error(request, "Error: formato no valido. El archivo debe tener extension xml")
+				return render(request,'formulario.html',{'form':form})
 
 			#Subimos el archivo
 			subirArchivo(request.FILES['partitura'])
 			#Ejecutamos los modulos del sistema experto
 			modulos(opciones,settings.MEDIA_ROOT+'.xml')
-			return render(request,'resultados.html')
+			#Mostramos los resultados
+			r_mod1,r_mod2,r_mod3 = mostrarResultados(opciones)
+			return render(request,'resultados.html',{'r_mod1':r_mod1, 'r_mod2':r_mod2, 'r_mod3':r_mod3})
 
 		else:
 			return HttpResponse('No es valido')
@@ -39,29 +43,35 @@ def subirArchivo(f):
 		dir_destino.write(chunk)
 	dir_destino.close()
 
-#Funcion auxiliar para mostrar los resultados del sistema experto
+#Funcion para mostrar los resultados del sistema experto
 def mostrarResultados(opciones):
 
-	r_mod1,r_mod2,r_mod3 = []
+	r_mod1 = False
+	r_mod2 = False
+	r_mod3 = False
 
 	#Leemos los fallos de los modulos activados
 	if('opcion1' in opciones):
+		r_mod1=[]
 		with open('./tfg_web/fallos/fallos_mod1','r') as f:
 			for l in f:
 				r_mod1.append(l)
 		f.close()
 
 	if('opcion2' in opciones):
+		r_mod2=[]
 		with open('./tfg_web/fallos/fallos_mod2','r') as f:
 			for l in f:
 				r_mod2.append(l)
 		f.close()
 
 	if('opcion3' in opciones):
+		r_mod3=[]
 		with open('./tfg_web/fallos/fallos_mod3','r') as f:
 			for l in f:
 				r_mod3.append(l)
 		f.close()
 
-	
+	return r_mod1,r_mod2,r_mod3
+
 		
