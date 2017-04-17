f6592e3956224c7b638812af0622e5c0a3c3d9f9
muscraziest <ltiradolopz@gmail.com>
2017-04-17 14:26:39 +0200

    Delete validar.py

--- tfg_django/tfg_web/validar.py
+++
@@ -1,9 +0,0 @@
-def validarExtension(f):
-	import os
-	from django.core.exceptions import ValidationError
-
-	ext = os.path.splitext(f.name)[1]
-	extension_valida = ['.xml']
-
-	if not ext.lower() in extension_valida:
-		raise ValidationError(u'Error: la extension debe ser xml')
